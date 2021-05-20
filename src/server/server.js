3f7313807189f15e08cefbc28450278fa0233cd4
tobiasafischer <40577304+tobiasafischer@users.noreply.github.com>
2021-05-19 18:01:00 -0700

    heroku

--- src/server/server.js
+++ src/server/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static('build'));
-const url = 'https://tobias-fischer-recipez.herokuapp.com/';
+const url = 'tobias-fischer-recipez.herokuapp.com';
 const mongoDB = `mongodb://${url}:27017/recipes`;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
